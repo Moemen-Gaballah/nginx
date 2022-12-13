2939763d26037aa1c4bdfbf6a1b223b887cc0d3e
moemen gaballa <moemengaballa@gmail.com>
2022-12-13 13:24:15 +0200

    nginx as a loadblancer with 3 node app

---
+++ app/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+const os = require("os")
+const hostname = os.hostname();
+
+app.get("/", (req, res) => res.send("Hello from " + hostname));
+
+app.listen(8888, () => console.log("listening on port 8888 on " + hostname ));
